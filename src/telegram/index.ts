1e8c6345a87cf09533caf01546ffce5f85f1ee5b
Bagautdin Omarov <38819496+Mergemat@users.noreply.github.com>
2026-06-08 18:52:01 +0300

    refactor: split telegram client into modules

---
+++ src/telegram/index.ts
@@ -0,0 +1,17 @@
+export { createTeleprotoClient } from "./client";
+export {
+  createTeleprotoDialogSource,
+  listDialogSummaries,
+  type DialogSource,
+  type FilterableDialogSummary,
+} from "./dialogs";
+export type {
+  Account,
+  CreateTelegramClient,
+  DialogSummary,
+  FireTgClient,
+  FolderSummary,
+  LoginParams,
+  MessageSummary,
+  SentMessage,
+} from "./types";
